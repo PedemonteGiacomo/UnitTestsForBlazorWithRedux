62338a49ccf7208a27ce627e71a2cba8a6e245ca
Giacomo Pedemonte <giacomo.pedemonte@esaote.com>
2024-03-15 10:32:38 +0100

    completed weather tests

--- WeatherActionsTests.cs
+++ WeatherActionsTests.cs
@@ -11,6 +11,7 @@ using Fluxor.Undo;
 using FluentAssertions;
 using static System.Net.WebRequestMethods;
 using System.Runtime.InteropServices;
+using System.IO;
 
 namespace UnitTestsForBlazorWithRedux
 {
@@ -69,7 +70,11 @@ namespace UnitTestsForBlazorWithRedux
                });
 
             var action2 = new WeatherLoadForecastsAction();
-            state.WithWeather(action2);
+            state = state.WithWeather(action2);
+
+            // so the forecasts should be not empty
+            state.Present.Forecasts.Should().NotBeEmpty(); //!!!
+            var forecasts = state.Present.Forecasts;
 
             var action3 = new WeatherSetLoadingAction(false);
             state = state.WithWeather(action3);
@@ -93,6 +98,228 @@ namespace UnitTestsForBlazorWithRedux
             // then check if the state is not loading anymore since it is initialized
             state.Present.Initialized.Should().BeTrue();
             state.Present.Loading.Should().BeFalse();
+
+
+            // now we can go back undoing actions
+            var action5 = new UndoAction<UndoableWeatherState>();
+            state = state.WithWeather(action5);
+
+            state.Should().BeEquivalentTo(
+               new UndoableWeatherState
+               {
+                   Past = new[]
+                   {
+                          new WeatherState()
+                          {
+                            Initialized = false,
+                            Loading = false,
+                            Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                                Initialized = false,
+                                Loading = true,
+                                Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                   },
+                   Present = new WeatherState()
+                   {
+                       Initialized = false,
+                       Loading = false,
+                       Forecasts = forecasts.ToArray()
+                   },
+                   Future = new[]
+                   {
+                        new WeatherState()
+                        {
+                           Initialized = true,
+                           Loading = false,
+                           Forecasts = forecasts.ToArray()
+                        }
+                   }
+               });
+
+            var action6 = new RedoAction<UndoableWeatherState>();
+            state = state.WithWeather(action6);
+
+            state.Should().BeEquivalentTo(
+               new UndoableWeatherState
+               {
+                   Past = new[]
+                   {
+                          new WeatherState()
+                          {
+                            Initialized = false,
+                            Loading = false,
+                            Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                                Initialized = false,
+                                Loading = true,
+                                Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = false,
+                               Forecasts = forecasts.ToArray()
+                          },
+                   },
+                   Present = new WeatherState()
+                   {
+                       Initialized = true,
+                       Loading = false,
+                       Forecasts = forecasts.ToArray()
+                   },
+               });
+
+            var action7 = new WeatherSetLoadingAction(true);
+            state = state.WithWeather(action7);
+
+            var action8 = new WeatherLoadForecastsAction();
+            state = state.WithWeather(action8);
+
+            var action9 = new WeatherSetLoadingAction(false);
+            state = state.WithWeather(action9);
+
+            var new_forecasts = state.Present.Forecasts;
+
+            state.Present.Forecasts.Should().NotBeEmpty();
+
+            // since we have performed a new action, the forecast should be different
+            state.Present.Forecasts.Should().BeEquivalentTo(new_forecasts);
+
+            var action10 = new UndoAllAction<UndoableWeatherState>();
+            state = state.WithWeather(action10);
+
+            state.Should().BeEquivalentTo(
+               new UndoableWeatherState
+               {
+
+                   Present = new WeatherState()
+                   {
+                       Initialized = false,
+                       Loading = false,
+                       Forecasts = Array.Empty<WeatherForecast>()
+                   },
+                   Future = new[]
+                   {
+                          new WeatherState()
+                          {
+                                Initialized = false,
+                                Loading = true,
+                                Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = false,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = false,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = true,
+                               Forecasts = new_forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = false,
+                               Forecasts = new_forecasts.ToArray()
+                          },
+                   }
+               });
+
+            var action11 = new RedoAllAction<UndoableWeatherState>();
+            state = state.WithWeather(action11);
+
+            state.Should().BeEquivalentTo(
+               new UndoableWeatherState
+               {
+                   Past = new[]
+                   {
+                          new WeatherState()
+                            {
+                                Initialized = false,
+                                Loading = false,
+                                Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                                Initialized = false,
+                                Loading = true,
+                                Forecasts = Array.Empty<WeatherForecast>()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = false,
+                               Loading = false,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = false,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = true,
+                               Forecasts = forecasts.ToArray()
+                          },
+                          new WeatherState()
+                          {
+                               Initialized = true,
+                               Loading = true,
+                               Forecasts = new_forecasts.ToArray()
+                          },
+                   },
+                   Present = new WeatherState()
+                   {
+                       Initialized = true,
+                       Loading = false,
+                       Forecasts = new_forecasts.ToArray()
+                   }
+               });
         }
     }
 }
